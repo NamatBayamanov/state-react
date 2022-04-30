b8545c296af2d04d8a253746ee1253aa86851d46
Namat Bayamanov <namataxvd15@gmail.com>
2022-04-30 23:28:37 +0600

    added Double component

--- src/components/Double/Double.js
+++ src/components/Double/Double.js
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import classes from "./Double.module.scss";
 
 
 function Double() {
+
+  const [charging, setCharging] = useState({
+    phone: "with mobile",
+    user: "Namat",
+  });
+
+  function onChargingChange() {
+    setCharging(charging.user = "Bakyt", charging.phone = "sumsung",);
+  };
+
+
+
+
   return ( 
     <>
       <div className={classes.Double}>
         <div className={classes.content}>
-          
+          <p>
+            I called my friend {charging.user} {charging.phone}.
+          </p>
+          <button onClick={onChargingChange}>
+            change information
+          </button>
         </div>
       </div>
     </>
